99fc542628d608bbbabd4bec90999a8f5308cb09
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-09-12 10:33:35 -0300

    Projeto Criado

---
+++ src/main/java/com/tcs/ProjetoBancoSpring/ProjetoBancoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.ProjetoBancoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBancoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBancoSpringApplication.class, args);
+	}
+
+}
