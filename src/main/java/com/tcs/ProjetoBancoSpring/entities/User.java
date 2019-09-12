a57edbdd84e10ac6fa7ca651bc8d50c0105cc87d
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-09-12 13:56:12 -0300

    user create

---
+++ src/main/java/com/tcs/ProjetoBancoSpring/entities/User.java
@@ -0,0 +1,102 @@
+package com.tcs.ProjetoBancoSpring.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String fname;
+    private String lname;
+    private String cpf;
+    private String pwd;
+    private String email;
+    private String tel;
+
+    public User() {
+
+    }
+
+    public User(Long id, String fname, String lname, String cpf, String pwd, String email, String tel) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.cpf = cpf;
+        this.pwd = pwd;
+        this.email = email;
+        this.tel = tel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", email='" + email + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/tcs/ProjetoBancoSpring/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tcs.ProjetoBancoSpring.repositories;
+
+import com.tcs.ProjetoBancoSpring.entities.User;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@SpringBootApplication
+public interface UserRepository extends JpaRepository<User, Long> {
+}
