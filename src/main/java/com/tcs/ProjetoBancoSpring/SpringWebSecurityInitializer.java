71192a5babb8329b4a57307ade535fdd9adef44c
GuilhermeNakahata <Guiilhermehsouza@gmail.com>
2019-09-13 09:19:15 -0300

    Configurações SpringSecurity

---
+++ src/main/java/com/tcs/ProjetoBancoSpring/SpringWebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.tcs.ProjetoBancoSpring;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringWebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
