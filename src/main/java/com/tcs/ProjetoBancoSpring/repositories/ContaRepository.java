dc61ab6082199ce409f05a6cfb0d03237b0916eb
Jhonny Ulisses <jhonnyulisilva@gmail.com>
2019-09-19 10:56:37 -0300

    Mudanças na Conta

--- src/main/java/com/tcs/ProjetoBancoSpring/repositories/ContaRepository.java
+++ src/main/java/com/tcs/ProjetoBancoSpring/repositories/ContaRepository.java
@@ -1,10 +1,15 @@
 package com.tcs.ProjetoBancoSpring.repositories;
 
 import com.tcs.ProjetoBancoSpring.entities.Conta;
+import com.tcs.ProjetoBancoSpring.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @SpringBootApplication
 public interface ContaRepository extends JpaRepository<Conta, Long>{
+
+    Conta findId(User idUser);
+
+    Boolean existFindId(User idUser);
 
 }
